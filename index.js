ae25421142498a5fe9977402e990fda4f68d23bf
Azim <annayev.azim@gmail.com>
2024-10-10 11:03:51 -0400

    finished the first one

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-//write your code here
+// Code 1
+const reverseString = (str) => {
+  let reversed = "";
+  for (let i = str.length - 1; i >= 0; i--) {
+    reversed += str[i];
+  }
+  return reversed;
+};
+// console.log(reverseString("hello")); //"olleh"
